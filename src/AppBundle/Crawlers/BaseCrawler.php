4304a0d58549427630862afcc5bb44e7f5134272
uveysservetoglu <uveysservetoglu@gmail.com>
2018-07-06 11:57:26 +0300

    Crawl image and download

--- src/AppBundle/Crawlers/BaseCrawler.php
+++ src/AppBundle/Crawlers/BaseCrawler.php
@@ -9,6 +9,8 @@ namespace AppBundle\Crawlers;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\File;
+use GuzzleHttp\Client as GuzzleClient;
 class BaseCrawler
 {
     public $kernel;
@@ -42,5 +44,52 @@ class BaseCrawler
         $crawler = new Crawler(file_get_contents($link));
         $filter = $crawler->filterXPath($xPathRule);
         return $filter->count() < 1 ?  null : $filter->attr($attr);
+    }
+    public function saveImage($folder, $link,$preView = null){
+
+        echo "[OK] The picture of the race is taken".PHP_EOL;
+        $media = null;
+
+        $imageDir = $this->rootDir.'/../web/upload/'.$folder;
+
+        if(!$this->fs->exists($imageDir)){
+            $this->fs->mkdir($imageDir );
+        }
+        $file = explode(".",$link);
+        $fileName = md5(microtime());
+        $target = $imageDir.'/'.$fileName;
+
+        $client = new GuzzleClient();
+        $client->get($link, array('save_to' => $target));
+
+        if (!$this->fs->exists($target)) {
+            throw new \Exception('Image of second hand vehicle could not saved to target : ' . $target);
+        }
+
+        $file = new File($target);
+        $mime = $file->getMimeType();
+        //Add extension to the file
+        $extension = 'png';
+        if (is_null($file->getExtension())) {
+            $arr = explode('.', $fileName);
+            if (count($arr) > 0) {
+                $extension = $arr[count($arr) - 1];
+            } else {
+                $arr = explode('/', $file->getMimeType());
+                if (count($arr) > 0) {
+                    $extension = $arr[1];
+                }
+            }
+        }
+        $source = $target;
+        $target .= '.' . $extension;
+        $this->fs->rename($source, $target);
+        $fileName .= '.'.$extension;
+        //$this->createThumbs($target,$fileName);
+
+        $media = array("url"=>$folder.'/'.$fileName, "type"=>"i", "mime"=>$mime);
+
+        return $media;
+
     }
 }
--- src/AppBundle/Crawlers/RaceSchedule.php
+++ src/AppBundle/Crawlers/RaceSchedule.php
@@ -12,15 +12,32 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class RaceSchedule extends BaseCrawler
 {
-    private function crawl(){
+    public function crawl(){
 
         self::getRace();
     }
 
     private function getRace(){
 
-        $url = "http://www.tjk.org/TR/YarisSever/Info/Page/GunlukYarisProgrami";
-        //$xPathRule = '//div[@class="container"]';
+        $url = "http://www.tjk.org/TR/YarisSever/Info/Sehir/GunlukYarisProgrami?SehirId=4&QueryParameter_Tarih=06%2F07%2F2018&SehirAdi=Bursa";
         $crawler = new Crawler(file_get_contents($url));
+
+        $xPathRule = '//div[@class="program"]/div[4]';
+        $nodeValue[]=$crawler->filterXPath($xPathRule)->each(function (Crawler $node){
+
+            self::races($node);
+
+        });
+    }
+
+    private function races($nodes){
+
+        echo "[OK] Races List GET".PHP_EOL;
+        $xPathRule = '//table[@class="tablesorter"]/tbody[1]/tr';
+        $nodeValue[]=$nodes->filterXPath($xPathRule)->each(function (Crawler $node){
+            $imgPath = $node->filterXPath('//td[1]/a/img');
+            $image   = parent::saveImage('', $imgPath->attr("src"));
+
+        });
     }
 }
